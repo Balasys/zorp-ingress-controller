ea52f7055129ca44747f441ab3bbcb13e542ee44
Balint Kovacs <blint@balasys.hu>
2019-09-09 12:04:02 +0200

    feat(zorp): run at startup

--- fs/start.sh
+++ fs/start.sh
@@ -20,5 +20,6 @@ if [ $# -gt 0 ] && [ "$(echo $1 | cut -b1-2)" != "--" ]; then
     # Probably a `docker run -ti`, so exec and exit
     exec "$@"
 else
+    /etc/init.d/zorp start
     exec python3 -m zorp_ic.controller "$@"
 fi
