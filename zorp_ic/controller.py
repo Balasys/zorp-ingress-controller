18e8cd054432386e3fc796dd43639c211228f037
Balint Kovacs <blint@balasys.hu>
2019-09-09 13:45:18 +0200

    feat(k8s): query endpoints and services

--- zorp_ic/controller.py
+++ zorp_ic/controller.py
@@ -3,9 +3,31 @@ import logging
 from apscheduler.schedulers.blocking import BlockingScheduler
 from .kubernetes_backend import KubernetesBackend
 
+
+class ZorpConfig():
+    def __init__(self, namespace='default', ingress_class='zorp', ingresses=None, services=None, endpoints=None, secrets=None):
+        self.ingresses = ingresses
+        self.services = services
+        self.endpoints = endpoints
+        self.secrets = secrets
+        self.k8s = KubernetesBackend(namespace, ingress_class)
+
+    def generate_config(self):
+        print(self.ingresses)
+        print(self.services)
+        print(self.endpoints)
+        print(self.secrets)
+
+    def load_k8s_config(self):
+        self.ingresses = self.k8s.get_relevant_ingresses()
+        self.services = self.k8s.get_relevant_services(self.ingresses)
+        print(zorpConfig.generate_config())
+        self.endpoints = self.k8s.get_relevant_endpoints(self.services)
+
 def process_k8s_changes(namespace, ingress_class):
-    k8s = KubernetesBackend(namespace, ingress_class)
-    print(k8s.get_relevant_ingresses())
+    zorpConfig = ZorpConfig(namespace, ingress_class)
+    zorpConfig.load_k8s_config()
+    print(zorpConfig.generate_config())
     return
 
 if __name__ == '__main__':
