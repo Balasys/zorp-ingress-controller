e45119db98944ca00409b2673a96133fa6eb8bb5
Balint Kovacs <blint@balasys.hu>
2019-09-12 13:38:38 +0200

    fix(controller): bad parameter, missing comma

--- zorp_ic/controller.py
+++ zorp_ic/controller.py
@@ -23,7 +23,7 @@ class ZorpConfig():
 
     def generate_self_signed_cert(self):
         self._logger.info("Generating self-signed certificate for default TLS service")
-        res = subprocess.Popen(['openssl', 'req', '-new', '-newkey', 'rsa:4096', '-days', '3650', '-sha256', '-nodes', '-x509', '-subj', '/CN=Ingress Default Certificate', '-keyout' '/etc/zorp/default-tls.key', '-out', '/etc/zorp/default-tls.cert'])
+        res = subprocess.Popen(['openssl', 'req', '-new', '-newkey', 'rsa:4096', '-days', '3650', '-sha256', '-nodes', '-x509', '-subj', '/CN=Ingress Default Certificate', '-keyout', '/etc/zorp/default-tls.key', '-out', '/etc/zorp/default-tls.cert'])
         output, error_ = res.communicate()
         if (error_):
             logger.error(error_)
