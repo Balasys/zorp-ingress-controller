2665219eae9ebc35be3cbedc7d2a56dac486fd38
Balint Kovacs <blint@balasys.hu>
2019-09-12 14:40:45 +0200

    feat(controller): config generation basics + minor fixes

--- zorp_ic/controller.py
+++ zorp_ic/controller.py
@@ -1,18 +1,62 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 import argparse
+from copy import deepcopy
+from jinja2 import Environment, FileSystemLoader
 import logging
+import os
 import os.path
 import subprocess
 from .kubernetes_backend import KubernetesBackend
 
 
-class ZorpConfig():
+class ZorpConfigGenerator(object):
+    def __init__(self, templatedir):
+        super(ZorpConfigGenerator, self).__init__()
+        self.env = Environment(
+            loader=FileSystemLoader(templatedir),
+            trim_blocks=True,
+            lstrip_blocks=True,
+            extensions=['jinja2.ext.do']
+        )
+
+    def renderTemplate(self, template, data, conf={'dst': '/etc/zorp/policy.py', 'mode': 644, 'owner': 'zorp', 'group': 'zorp'}):
+        mode = int(str(conf.get('mode', '644')), 8)
+        owner = conf.get('owner', None)
+        group = conf.get('group', None)
+        try:
+            t = self.env.get_template(template)
+        except IOError as e:
+            sys.exit("Error opening template file: %s" % e)
+        confdir = os.path.dirname(conf['dst'])
+        if not os.path.exists(confdir):
+            try:
+                Path(confdir).mkdir(mode | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH, parents=True)
+            except (IOError, FileExistsError, OSError) as e:
+                sys.exit("Error creating %s: %s" % (confdir, e))
+            if owner or group:
+                shutil.chown(confdir, user=owner, group=group)
+        try:
+            rendered = t.render(conf=data, env=os.environ)
+        except BaseException as e:
+            sys.exit("Error rendering template: %s" % e)
+        try:
+            with open(conf['dst'], 'w') as outfile:
+                outfile.write(rendered)
+        except IOError as e:
+            sys.exit("Error writing to destination file: %s" % e)
+        os.chmod(conf['dst'], mode)
+        if owner or group:
+            shutil.chown(conf['dst'], user=owner, group=group)
+
+class ZorpConfig(object):
+
+    config = {}
 
     def __init__(self, namespace='default', ingress_class='zorp', behaviour='basic', ingresses=None, services=None, endpoints=None, secrets=None):
-        self.ingresses = ingresses
-        self.services = services
-        self.endpoints = endpoints
-        self.secrets = secrets
+        self.config["ingresses"] = ingresses
+        self.config["services"] = services
+        self.config["endpoints"] = endpoints
+        self.config["secrets"] = secrets
         self.behaviour = behaviour
 
         self._logger = logging.getLogger('flask.app')
@@ -22,38 +66,38 @@ class ZorpConfig():
         self.has_default_cert = os.path.isfile('/etc/zorp/default-tls.key')
 
     def generate_self_signed_cert(self):
-        self._logger.info("Generating self-signed certificate for default TLS service")
+        self._logger.error("Generating self-signed certificate for default TLS service")
         res = subprocess.Popen(['openssl', 'req', '-new', '-newkey', 'rsa:4096', '-days', '3650', '-sha256', '-nodes', '-x509', '-subj', '/CN=Ingress Default Certificate', '-keyout', '/etc/zorp/default-tls.key', '-out', '/etc/zorp/default-tls.cert'])
         output, error_ = res.communicate()
         if (error_):
             logger.error(error_)
+        else:
+            self.has_default_cert = True
 
     def generate_config(self):
         if len(self.secrets) == 0 and self.has_default_cert is False and self.behaviour == 'basic':
             self.generate_self_signed_cert()
+        policyPy = ZorpConfigGenerator("templates/")
         f = open("/tmp/k8s-config", "w")
-        f.write(str(self.ingresses)+"\n")
-        f.write(str(self.services)+"\n")
-        f.write(str(self.endpoints)+"\n")
-        f.write(str(self.secrets)+"\n")
+        f.write(str(self.config)+"\n")
         f.close()
 
     def reload_zorp(self):
+        self._logger.info("Configuration changed, reloading Zorp")
         res = subprocess.Popen(['/usr/sbin/zorpctl', 'reload'], stdout=subprocess.PIPE)
         output, error_ = res.communicate()
         if (error_):
             logger.error(error_)
 
     def load_k8s_config(self):
-        oldconfig = {'ingresses': self.ingresses, 'services': self.services, 'endpoints': self.endpoints, 'secrets': self.secrets}
-        self.ingresses = self.k8s.get_relevant_ingresses()
-        self.services = self.k8s.get_relevant_services(self.ingresses)
-        self.endpoints = self.k8s.get_relevant_endpoints(self.services)
+        oldconfig = deepcopy(self.config)
+        self.config["ingresses"] = self.k8s.get_relevant_ingresses()
+        self.config["services"] = self.k8s.get_relevant_services(self.config["ingresses"])
+        self.config["endpoints"] = self.k8s.get_relevant_endpoints(self.config["services"])
         self.secrets= {}
         for secret_name in self.k8s.list_secrets():
             self.secrets[secret_name] = self.k8s.get_secret(secretname)
-        if (oldconfig['ingresses'] != self.ingresses or oldconfig['services'] != self.services or
-                oldconfig['endpoints'] != self.endpoints or oldconfig['secrets'] != self.secrets):
+        if oldconfig != self.config:
            self.generate_config()
            self.reload_zorp()
 
