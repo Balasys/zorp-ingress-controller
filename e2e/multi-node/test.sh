bc5562c9317466cad075aa475e111423d8fb7640
Balint Kovacs <blint@balasys.hu>
2019-09-27 11:12:49 +0200

    debug(e2e): show active sockets

--- e2e/multi-node/test.sh
+++ e2e/multi-node/test.sh
@@ -5,7 +5,10 @@ cd $SCRIPT_ROOT
 
 . deploy.sh
 
-echo -e "\n WAITING for controller to pick up configuration\n"
+echo -e "\n WAITING for controller to pick up configuration...\n"
 sleep 15
+
+echo -n "\n ACTIVE sockets:\n"
+netstat -antup
 
 . run_test.sh
