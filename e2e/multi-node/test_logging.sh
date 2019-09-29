1371ca194b443ecff4c575095619737f7abb4acb
Balint Kovacs <blint@balasys.hu>
2019-09-29 15:42:00 +0200

    fix(e2e): start kubectl logs in the background

--- e2e/multi-node/test_logging.sh
+++ e2e/multi-node/test_logging.sh
@@ -6,5 +6,5 @@ cd $SCRIPT_ROOT
 
 zorp_pods=$(kubectl get pods -l run=zorp-ingress -n zorp-controller | awk '/zorp/ {print $1}')
 while read -r pod; do
-    kubectl logs -f pod/$pod -n zorp-controller >> controller-$pod.log
+    kubectl logs -f pod/$pod -n zorp-controller & >> controller-$pod.log
 done <<< "$zorp_pods"
