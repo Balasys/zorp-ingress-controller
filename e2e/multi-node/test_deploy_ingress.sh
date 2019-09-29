327866913ead5883ed73a392d0a9fa739c4746b5
Balint Kovacs <blint@balasys.hu>
2019-09-29 15:54:30 +0200

    fix(e2e): only delete replaceable parts of deployment

--- e2e/multi-node/test_deploy_ingress.sh
+++ e2e/multi-node/test_deploy_ingress.sh
@@ -4,7 +4,7 @@ set -e
 SCRIPT_ROOT=$(dirname "${BASH_SOURCE}")
 cd $SCRIPT_ROOT
 
-kubectl delete -f zorp-ingress.yaml
+kubectl delete -f replace-zorp-ingress.yaml
 kubectl apply -f replace-zorp-ingress.yaml
 
 zorp_pods=$(kubectl get pods -l run=zorp-ingress -n zorp-controller | awk '/zorp/ {print $1}')
